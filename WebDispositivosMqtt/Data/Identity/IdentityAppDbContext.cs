a5a2a1431975e51a539e00396d2d989ef0b27e18
eloyc <eloy.canchanya@outlook.es>
2026-05-04 18:07:48 -0500

    Se mueve tablas de identity a esquema identity

--- WebDispositivosMqtt/Data/Identity/IdentityAppDbContext.cs
+++ WebDispositivosMqtt/Data/Identity/IdentityAppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebDispositivosMqtt.Data.Identity.Models;
 
@@ -7,6 +8,18 @@ namespace WebDispositivosMqtt.Data.Identity
     public class IdentityAppDbContext : IdentityDbContext<ApplicationUser>
     {
         public IdentityAppDbContext(DbContextOptions<IdentityAppDbContext> options) : base(options) { }
-    }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers", "identity");
+            builder.Entity<IdentityRole>().ToTable("AspNetRoles", "identity");
+            builder.Entity<IdentityUserRole<string>>().ToTable("AspNetUserRoles", "identity");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("AspNetUserClaims", "identity");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("AspNetUserLogins", "identity");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("AspNetRoleClaims", "identity");
+            builder.Entity<IdentityUserToken<string>>().ToTable("AspNetUserTokens", "identity");
+        }
+    }
 }
--- WebDispositivosMqtt/Migrations/20260504225241_MoveIdentityTablesToIdentitySchema.Designer.cs
+++ WebDispositivosMqtt/Migrations/20260504225241_MoveIdentityTablesToIdentitySchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebDispositivosMqtt.Data.Identity;
 
@@ -11,9 +12,11 @@ using WebDispositivosMqtt.Data.Identity;
 namespace WebDispositivosMqtt.Migrations
 {
     [DbContext(typeof(IdentityAppDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260504225241_MoveIdentityTablesToIdentitySchema")]
+    partial class MoveIdentityTablesToIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
