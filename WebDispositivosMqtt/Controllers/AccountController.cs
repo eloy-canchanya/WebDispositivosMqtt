84c7736e31aa95156b489efd0c73a7a7b52056d1
eloyc <eloy.canchanya@outlook.es>
2026-05-05 11:11:58 -0500

    se agrega contexto de la base de datos y se organiza contexto de identity

--- WebDispositivosMqtt/Controllers/AccountController.cs
+++ WebDispositivosMqtt/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using WebDispositivosMqtt.Identity;
+using WebDispositivosMqtt.DataIdentity.Models;
 using WebDispositivosMqtt.Models;
 
 namespace WebDispositivosMqtt.Controllers
--- WebDispositivosMqtt/Controllers/UsersController.cs
+++ WebDispositivosMqtt/Controllers/UsersController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using WebDispositivosMqtt.Identity;
+using WebDispositivosMqtt.DataIdentity.Models;
 using WebDispositivosMqtt.Models;
 
 namespace WebDispositivosMqtt.Controllers
