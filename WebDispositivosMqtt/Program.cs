daf15bd1e610e427b423bb40a9c78727f74c77c1
eloyc <eloy.canchanya@outlook.es>
2026-06-21 13:29:38 -0500

    Se maneja las credenciales de firebase desde una archivo (dev) o variables de entorno.

--- WebDispositivosMqtt/Program.cs
+++ WebDispositivosMqtt/Program.cs
@@ -117,8 +117,9 @@ namespace WebDispositivosMqtt
                 });
 
             // Firebase FCM
-            var firebaseCredential = GoogleCredential
-                .FromFile("clorador-alertas-firebase-adminsdk-fbsvc-e0bb8f1385.json")
+            var firebaseCredential = (Environment.GetEnvironmentVariable("FIREBASE_CREDENTIALS_JSON") is { } firebaseJson
+                ? GoogleCredential.FromJson(firebaseJson)
+                : GoogleCredential.FromFile("clorador-alertas-firebase-adminsdk-fbsvc-e0bb8f1385.json"))
                 .CreateScoped(
                     "https://www.googleapis.com/auth/cloud-platform",
                     "https://www.googleapis.com/auth/firebase.messaging"
